08a84ab004628e45fdf7935649fc6b96ba878cfd
Bill-Pill <delashmuttwa@gmail.com>
2022-01-02 16:12:12 -0500

    Restructure to use JS objects

--- js/site.js
+++ js/site.js
@@ -2,47 +2,52 @@
 // Controller Function
 function getValue() {
     document.getElementById("alert").classList.add("invisible");
-
+    // Get user string
     let userString = document.getElementById("userString").value;
-
-    // Set input string to lower case and cleanup input string of any whitespace, special characters, and numbers.
-    let cleanedString = userString.toLowerCase()
-    cleanedString = cleanedString.replace(/([^a-z])/g, '')
-    cleanedString = cleanedString.trim()
-
-    // Compare strings for palindrome check
-    let reversedString = revString(cleanedString);
-    let isPalindrome = (reversedString == cleanedString)
-    
-    let message = ""
-    if (isPalindrome) {
-        message = "Congratulations! You entered a palindrome."
-    } else {
-        message = "Nope. Your input isn't a palindrome."
-    }
-    displayString(reversedString,message);
+    // Check palindrome
+    let returnObj = checkPalindrome(userString)
+    // Display result on page
+    displayString(returnObj);
 }
 
-// Reverse string for palindrome check
+// Reverse string and perform palindrome check
 // Logic Function
-function revString(userString) {
-    
+function checkPalindrome(userString) {
+
+    // Set input string to lower case and cleanup input string of any whitespace, special characters, and numbers.
+    let cleanedString = userString.toLowerCase();
+    let regex = /([^a-z0-9])/gi;
+    cleanedString = cleanedString.replace(regex, "")
+
     let revString = [];
-    // Reverse userString string via for loop
-    for (let index = userString.length - 1; index >= 0; index--) {
-        revString += userString[index];
+    let returnObj = {};
+
+    // Reverse cleanedString string via for loop
+    for (let index = cleanedString.length - 1; index >= 0; index--) {
+        revString += cleanedString[index];
     }
-    return revString;
+
+    // Set object property based on palindrome status
+    if (revString == cleanedString) {
+        returnObj.msg = "Congratulations! You entered a palindrome."
+    } else {
+        returnObj.msg = "Sorry. Your input isn't a palindrome."
+
+    }
+
+    returnObj.reversed = revString;
+
+    return returnObj;
 }
 
 // Display reversed string to user
 // View Function
-function displayString(revString, message) {
+function displayString(returnObj) {
 
     // Write message to heading
-    document.getElementById("heading").innerHTML = message;
+    document.getElementById("heading").innerHTML = returnObj.msg;
     // Write message to page
-    document.getElementById("msg").innerHTML = `Your word/phrase reversed is: ${revString}`;
+    document.getElementById("msg").innerHTML = `Your word/phrase reversed is: ${returnObj.reversed}`;
     // Show alert box
     document.getElementById("alert").classList.remove("invisible");
 }
